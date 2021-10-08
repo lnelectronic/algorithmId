a4b97fb65246d17e9483e1f8914af3a20f965bcf
lnelectronic <ln.electronic4@gmail.com>
2021-10-08 07:52:21 +0700

    start algorithm

---
+++ main.go
@@ -0,0 +1,14 @@
+// ---------------------------------------------------------------------------
+// LN-ELECTRONIC PROJECT LN-16C10R
+// wwww.ln-electronic.com  ProjectManager : @Kimera
+// FileData: 8/10/2564 7:37 2564  FileName : main.go
+// ---------------------------------------------------------------------------
+
+package algorithmId
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("hello algorithmId")
+}
